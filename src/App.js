6ccbd743f7747ea3c23eb387f8e43429c50f9ce5
Adetula Tolulope <adetulatolu@gmail.com>
2019-08-21 21:36:05 +0100

    Separated Components, styled the homepage better.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div >
       <HomePage>
-        
+
       </HomePage>
     </div>
   );
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React  from 'react';
+
+import '../homepage/homepage.styles.scss'
+import {Directory} from '../../components/directory/directory.component'
+
+const HomePage = () => (
+ 
+    <Directory className='homepage'>
+        
+    </Directory>
+   
+
+)
+
+export default HomePage
